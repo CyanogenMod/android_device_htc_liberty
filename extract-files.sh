5c7910e105d14242c8cd16aa8ca917ffecea38f4
attn1 <attn1.repo@gmail.com>
2010-08-08 22:29:25 -0400

    fix extract-files

--- extract-files.sh
+++ extract-files.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+DEVICE=liberty
+
 mkdir -p ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/akmd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/wifitools ../../../vendor/htc/$DEVICE/proprietary
