5cd2648223373675578d4759f172e923f5713bd9
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-10-15 23:08:33 +0800

    liberty: Override dirty_ratio on UMS
    Change-Id: I3771703c68e7d31556880aa5fd12ac1bca8d2418

--- liberty.mk
+++ liberty.mk
@@ -76,6 +76,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.bt.name=Android \
     ro.config.sync=yes
 
+# Override /proc/sys/vm/dirty_ratio on UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
