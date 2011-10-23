ce970b8bb332616857c58ff751145924036225e3
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-10-24 07:30:10 +0800

    liberty: recovery: Do not light up cap keys when charging in offmode
    Change-Id: I995e7c081e29227e06be1bfcc2e202f99393ad26

--- recovery/utilities/postrecoveryboot.sh
+++ recovery/utilities/postrecoveryboot.sh
@@ -2,4 +2,16 @@
 
 sleep 2
 
-echo 200 > /sys/class/leds/button-backlight/brightness
+# on Liberty, `ps` while power on:
+#   73 root       436 S    /sbin/offmode_charging
+#   74 root       252 S    /sbin/detect_key
+#
+# while power off:
+#   73 root       444 S    /sbin/offmode_charging
+#   74 root       264 S    /sbin/detect_key
+#
+if [ ! -x /sbin/offmode_charging ] \
+|| [ "$(ps | grep '436 S */sbin/offmode_charging')" ]
+then
+    echo 200 > /sys/class/leds/button-backlight/brightness
+fi
