ae34df9772bcab1b1f8b68822dd1d03e907b6905
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-10-26 06:50:16 +0800

    liberty: recovery: Simply lights up cap keys by init.rc
    Change-Id: I9ea8df49996d8088abd0727d2fc44e0525a7a869

--- recovery/utilities/Android.mk
+++ recovery/utilities/Android.mk
@@ -2,14 +2,6 @@ ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),liberty)
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := postrecoveryboot.sh
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 # offmode charging
 
 include $(CLEAR_VARS)
